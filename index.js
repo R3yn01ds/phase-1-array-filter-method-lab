7c60113500647cd4d5c2f0bf3de6610c43a69cf2
johnsey <johnsey.reynolds@gmail.com>
2022-04-25 13:31:26 -0400

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, attribute) {
+    return drivers.filter(function (driver) {
+        return driver.toLowerCase() === attribute.toLowerCase()
+    })
+}
+function fuzzyMatch(driver,attribute) {
+    return driver.filter(function (driver) {
+        return driver.toLowerCase().substring(0, attribute.length) === attribute.toLowerCase()
+    })
+}
+function matchName(drivers, argument) {
+    return drivers.filter(function (driver) { 
+        return driver.name === argument })
+}
